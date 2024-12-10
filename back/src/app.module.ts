ba7dff6fa7f112de95775eacc0814193e5c93a9c
leoxh <leoxh@hotmail.com>
2024-12-09 20:10:39 -0500

    added initial config and entities

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import typeorm from './config/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [typeorm],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        ...config.get('typeorm'),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- back/src/main.ts
+++ back/src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+//import { LoggerMiddleware } from './middlewares/logger.middleware';
+//import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  
+  const swaggerConfig = new DocumentBuilder()
+  .setTitle("RentaFacil")
+  .setDescription("This is RentaFacil Backend")
+  .setVersion('1.0')
+  .addBearerAuth()
+  .build()
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig)
+  SwaggerModule.setup('api', app, document, { customSiteTitle: 'Documents backend RentaFacil' });
+  
+  //const loggerMiddleware = new LoggerMiddleware();
+  //app.use(loggerMiddleware.use);
+  //app.useGlobalPipes(new ValidationPipe());
+
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
