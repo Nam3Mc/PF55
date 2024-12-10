d3b118d5c1331282fc5c4f089126c618893ee8be
namemc95 <moralesdreiser5@gmail.com>
2024-12-09 22:36:34 -0500

    agrege algunos dtos

--- back/src/modules/account/account.service.ts
+++ back/src/modules/account/account.service.ts
@@ -1,26 +1,17 @@
 import { Injectable } from '@nestjs/common';
-import { CreateAccountDto } from './dto/create-account.dto';
-import { UpdateAccountDto } from './dto/update-account.dto';
+import { AccountRepository } from './account.repository';
 
 @Injectable()
 export class AccountService {
-  create(createAccountDto: CreateAccountDto) {
-    return 'This action adds a new account';
+  constructor(
+    private readonly accountRepository: AccountRepository
+  ) {}
+  
+  findById(id: string) {
+    return this.accountRepository.getAccountById(id)
   }
 
   findAll() {
     return `This action returns all account`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} account`;
-  }
-
-  update(id: number, updateAccountDto: UpdateAccountDto) {
-    return `This action updates a #${id} account`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} account`;
   }
 }
