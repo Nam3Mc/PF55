e4f3e64534e6fe736da4c4774504044f72975eb1
namemc95 <moralesdreiser5@gmail.com>
2025-02-10 22:00:50 -0500

    deployong into railway

--- back/src/config/config.ts
+++ back/src/config/config.ts
@@ -6,16 +6,18 @@ dotenvConfig({ path: '.env' });
 
 const config = {
     type: 'postgres',
-    host: process.env.DATABASE_HOST || 'localhost',
-    port: parseInt(process.env.DATABASE_PORT ),
-    username: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
+    // host: process.env.DATABASE_HOST || 'localhost',
+    // port: parseInt(process.env.DATABASE_PORT ),
+    // username: process.env.DATABASE_USER,
+    // password: process.env.DATABASE_PASSWORD,
+    // database: process.env.DATABASE_NAME,
+    url: process.env.DATABASE_URL,
     entities: ['dist/**/*.entity{.ts,.js}'],
     migrations: ['dist/**/migrations/*{.ts,.js}'],
     autoLoadEntities: true,
     dropSchema: false,
-    synchronize: true
+    synchronize: true,
+    ssl: process.env.NODE_ENV === 'production' ? {rejectUnauthorized: false } : false
 };
 
 
